22b723515bfd800c53f1342c0140171bf801aef2
kazutoki-nawamaki-lvgs <kazutoki.nawamaki@leverages.jp>
2024-03-19 21:42:15 +0900

    バグの修正

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -14,7 +14,7 @@ export default async function Page({
     page?: string;
   };
 }) {
-  const query = searchParams?.query;
+  const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
   return (
